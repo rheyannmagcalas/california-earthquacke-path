c5c3b55303dcb0df91b84088242e8aefafd891fb
rheyannmagcalas <46052282+rheyannmagcalas@users.noreply.github.com>
2021-09-02 21:12:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -301,7 +301,7 @@ if add_selectbox == 'Query':
                     
                     lat, long, coords = getCoordinatesOfPointsInPath(path_info[0])
                     
-                    folium.Marker([coords[0][0], coords[0][1]], icon=folium.Icon(color="blue", icon="home", prefix='fa')).add_to(m)
+                    folium.Marker([coords[0][0], coords[0][1]], icon=folium.Icon(color="green", prefix='fa')).add_to(m)
                         
                     folium.PolyLine(coords, popup='<b>Path of Vehicle_1</b>',
                                                         tooltip='Vehicle_1',
